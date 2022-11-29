c7d922482697b06114e4740d16b1c5cbf99a07d5
englishgritts <sjcarroll238@yahoo.com>
2022-11-28 17:41:32 -0700

    Added remove color functionality. Currently only one box at a time.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const container = document.querySelector('.container');
 let grid = 16;
 
-for (i=0; i <= (grid * grid); i++) {
-    const grid = document.createElement('div');
-    grid.addEventListener('mouseover', colorBlack)
-    container.appendChild(grid);
-
+function changeColor() {
+    for (i=0; i <= (grid * grid); i++) {
+        const grid = document.createElement('div');
+        grid.addEventListener('mouseover', colorBlack)
+        container.appendChild(grid);
+    
+    }
 }
 
 function colorBlack(e) {
     e.target.classList.add('black');
 }
+
+function clearButton () {
+    let clear = document.getElementById('clear');
+    clear.addEventListener('click', removeColor)
+}
+
+function removeColor() {
+    let element = document.querySelector('.black');
+    element.classList.remove('black');
+}
+
+changeColor();
+clearButton();
